bb418c84969e5696c713795c1018e2aa4e406875
Jeff <jeffinho2002@live.com>
2023-09-20 14:38:19 -0300

    att de ultima hora

--- CHAT/msg.js
+++ CHAT/msg.js
@@ -1,24 +1,12 @@
 function adicionaItem(nomeItem, qtdItem) {
   const novoItem = document.createElement(`li`);
   novoItem.innerHTML = `<div class="chatUsuario">
-                        <h5 class="labelUsuario">Você diz:</h5>\n
-                        <h4 class="h4Usuario">${nomeItem}</h4>
+                        <h5 class="labelUsuario">Você disse:</h5>\n
+                        <p class="MSGusuario">${nomeItem}</p>
                         </div>`;
 
-  const listaCompras = document.querySelector("#lista-de-compras");
+  const listaCompras = document.querySelector("#mensagem");
   listaCompras.appendChild(novoItem);
 
 }
 
-function formAdiciona(event) {
-  event.preventDefault();
-
-  const campoItem = document.querySelector("#item");
-  // se eu chegar aqui, o código está validado...
-  adicionaItem(campoItem.value);
-
-  campoItem.value = "";
-}
-
-const formAdd = document.querySelector("#formAdd");
-formAdd.addEventListener("submit", formAdiciona);
